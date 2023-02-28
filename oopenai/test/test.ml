69c6bf4d9847d4cc5b9f2c021a19cb6651e35f42
Freda Xin <freda.xin@gmail.com>
2023-02-27 20:23:54 -0500

    Modify packages, modify test for auth

--- oopenai/test/test.ml
+++ oopenai/test/test.ml
@@ -1,1 +1,9 @@
-let () = assert(true)
+open Oopenai
+
+let create_completion_request_t = 
+  let req = Create_completion_request.create "ada" in 
+  let prompt = Some ["This is a test prompt."] in 
+  let n = Some 5l in
+  {req with prompt;n}
+
+let _ = Lwt_main.run (Open_ai_api.create_completion ~create_completion_request_t)
