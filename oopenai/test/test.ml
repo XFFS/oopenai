a4996baeb7cb54ee33f207ae936a7aa5bfac1189
Shon Feder <shon.feder@gmail.com>
2023-03-02 11:23:57 -0500

    Fix tests

--- oopenai/test/test.ml
+++ oopenai/test/test.ml
@@ -2,8 +2,10 @@ open Oopenai
 
 let create_completion_request_t = 
   let req = Create_completion_request.create "ada" in 
-  let prompt = Some ["This is a test prompt."] in 
+  let prompt = Some ["Give me dogs"] in
   let n = Some 5l in
-  {req with prompt;n}
+  {req with prompt; n}
 
-let _ = Lwt_main.run (Open_ai_api.create_completion ~create_completion_request_t)
+let () =
+  let resp = Lwt_main.run (Open_ai_api.create_completion ~create_completion_request_t) in
+  assert ((List.length resp.choices) = 5)
