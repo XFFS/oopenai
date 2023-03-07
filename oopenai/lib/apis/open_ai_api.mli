0a65853e56f6f61211edff3eff007be30e4e2ef8
Freda Xin <freda.xin@gmail.com>
2023-03-06 21:03:16 -0500

    Abstract authorization

--- oopenai/lib/apis/open_ai_api.mli
+++ oopenai/lib/apis/open_ai_api.mli
@@ -5,24 +5,29 @@
  *
  *)
 
-val cancel_fine_tune : fine_tune_id:string -> Fine_tune.t Lwt.t
-val create_completion : create_completion_request_t:Create_completion_request.t -> Create_completion_response.t Lwt.t
-val create_edit : create_edit_request_t:Create_edit_request.t -> Create_edit_response.t Lwt.t
-val create_embedding : create_embedding_request_t:Create_embedding_request.t -> Create_embedding_response.t Lwt.t
-val create_file : file:string -> purpose:string -> Open_ai_file.t Lwt.t
-val create_fine_tune : create_fine_tune_request_t:Create_fine_tune_request.t -> Fine_tune.t Lwt.t
-val create_image : create_image_request_t:Create_image_request.t -> Images_response.t Lwt.t
-val create_image_edit : image:string -> prompt:string -> ?mask:string -> ?n:int32 -> ?size:Enums.size -> ?response_format:Enums.response_format -> ?user:string -> unit -> Images_response.t Lwt.t
-val create_image_variation : image:string -> ?n:int32 -> ?size:Enums.size -> ?response_format:Enums.response_format -> ?user:string -> unit -> Images_response.t Lwt.t
-val create_moderation : create_moderation_request_t:Create_moderation_request.t -> Create_moderation_response.t Lwt.t
-val create_search : engine_id:string -> create_search_request_t:Create_search_request.t -> Create_search_response.t Lwt.t
-val delete_file : file_id:string -> Delete_file_response.t Lwt.t
-val delete_model : model:string -> Delete_model_response.t Lwt.t
-val download_file : file_id:string -> string Lwt.t
-val list_files : unit -> List_files_response.t Lwt.t
-val list_fine_tune_events : fine_tune_id:string -> ?stream:bool -> unit -> List_fine_tune_events_response.t Lwt.t
-val list_fine_tunes : unit -> List_fine_tunes_response.t Lwt.t
-val list_models : unit -> List_models_response.t Lwt.t
-val retrieve_file : file_id:string -> Open_ai_file.t Lwt.t
-val retrieve_fine_tune : fine_tune_id:string -> Fine_tune.t Lwt.t
-val retrieve_model : model:string -> Model.t Lwt.t
+
+module Make (Config: Request.Auth): sig
+  
+    val cancel_fine_tune : fine_tune_id:string -> Fine_tune.t Lwt.t
+    val create_completion : create_completion_request_t:Create_completion_request.t -> Create_completion_response.t Lwt.t
+    val create_edit : create_edit_request_t:Create_edit_request.t -> Create_edit_response.t Lwt.t
+    val create_embedding : create_embedding_request_t:Create_embedding_request.t -> Create_embedding_response.t Lwt.t
+    val create_file : file:string -> purpose:string -> Open_ai_file.t Lwt.t
+    val create_fine_tune : create_fine_tune_request_t:Create_fine_tune_request.t -> Fine_tune.t Lwt.t
+    val create_image : create_image_request_t:Create_image_request.t -> Images_response.t Lwt.t
+    val create_image_edit : image:string -> prompt:string -> ?mask:string -> ?n:int32 -> ?size:Enums.size -> ?response_format:Enums.response_format -> ?user:string -> unit -> Images_response.t Lwt.t
+    val create_image_variation : image:string -> ?n:int32 -> ?size:Enums.size -> ?response_format:Enums.response_format -> ?user:string -> unit -> Images_response.t Lwt.t
+    val create_moderation : create_moderation_request_t:Create_moderation_request.t -> Create_moderation_response.t Lwt.t
+    val create_search : engine_id:string -> create_search_request_t:Create_search_request.t -> Create_search_response.t Lwt.t
+    val delete_file : file_id:string -> Delete_file_response.t Lwt.t
+    val delete_model : model:string -> Delete_model_response.t Lwt.t
+    val download_file : file_id:string -> string Lwt.t
+    val list_files : unit -> List_files_response.t Lwt.t
+    val list_fine_tune_events : fine_tune_id:string -> ?stream:bool -> unit -> List_fine_tune_events_response.t Lwt.t
+    val list_fine_tunes : unit -> List_fine_tunes_response.t Lwt.t
+    val list_models : unit -> List_models_response.t Lwt.t
+    val retrieve_file : file_id:string -> Open_ai_file.t Lwt.t
+    val retrieve_fine_tune : fine_tune_id:string -> Fine_tune.t Lwt.t
+    val retrieve_model : model:string -> Model.t Lwt.t
+
+end
