41b68903c7fd031c71117f980dd346da46b07caf
Shon Feder <shon.feder@gmail.com>
2023-03-17 13:56:46 -0400

    Fix tests
    - Fix hardcoded filename
    - Fix disabled tests running (by wrapping all test bodies in a thunk)

--- oopenai/test/test.ml
+++ oopenai/test/test.ml
@@ -12,12 +12,12 @@ let free () =
   print_endline "freeing all resources";
   Lwt.return ()
 
-let test_lwt (test_case : bool Lwt.t) switch () =
+let test_lwt (test_case : unit -> bool Lwt.t) switch () =
   Lwt_switch.add_hook (Some switch) free;
-  let+ result = test_case in
+  let+ result = test_case () in
   Alcotest.(check bool) "should be true" true result
 
-let test name (test_case : bool Lwt.t) : unit Alcotest_lwt.test_case =
+let test name (test_case : unit -> bool Lwt.t) : unit Alcotest_lwt.test_case =
   Alcotest_lwt.test_case name `Quick (test_lwt test_case)
 
 let tests =
@@ -25,87 +25,96 @@ let tests =
     , test
         "can create_completion"
         begin
-          let create_completion_request_t =
-            let req = Create_completion_request.create "ada" in
-            let prompt = Some [ "Give me dogs"; "Give me some cats" ] in
-            let n = Some 5l in
-            { req with prompt; n }
-          in
-          let+ resp = API.create_completion ~create_completion_request_t in
-          List.length resp.choices = 10
+          fun () ->
+            let create_completion_request_t =
+              let req = Create_completion_request.create "ada" in
+              let prompt = Some [ "Give me dogs"; "Give me some cats" ] in
+              let n = Some 5l in
+              { req with prompt; n }
+            in
+            let+ resp = API.create_completion ~create_completion_request_t in
+            List.length resp.choices = 10
         end )
   ; ( `Disabled
     , test
         "can create_edit"
         begin
-          let create_edit_request_t =
-            let req =
-              Create_edit_request.create
-                "text-davinci-edit-001"
-                "Fix the spelling mistakes"
+          fun () ->
+            let create_edit_request_t =
+              let req =
+                Create_edit_request.create
+                  "text-davinci-edit-001"
+                  "Fix the spelling mistakes"
+              in
+              let input = Some "What day of the wek is it?" in
+              { req with input }
             in
-            let input = Some "What day of the wek is it?" in
-            { req with input }
-          in
-          let+ resp = API.create_edit ~create_edit_request_t in
-          match resp.choices with
-          | corrected :: _ ->
-              corrected.text != Some "What day of the wek is it?"
-          | [] -> false
+            let+ resp = API.create_edit ~create_edit_request_t in
+            match resp.choices with
+            | corrected :: _ ->
+                corrected.text != Some "What day of the wek is it?"
+            | [] -> false
         end )
   ; ( `Disabled
     , test
         "can create_embedding"
         begin
-          let create_embedding_request_t =
-            let input = [ "Are cats cool or cooler?" ] in
-            Create_embedding_request.create "text-embedding-ada-002" input
-          in
-          let+ resp = API.create_embedding ~create_embedding_request_t in
-          List.length resp.data != 0
+          fun () ->
+            let create_embedding_request_t =
+              let input = [ "Are cats cool or cooler?" ] in
+              Create_embedding_request.create "text-embedding-ada-002" input
+            in
+            let+ resp = API.create_embedding ~create_embedding_request_t in
+            List.length resp.data != 0
         end )
   ; ( `Disabled
     , test
         "can create_file"
         begin
-          let file =
-            In_channel.with_open_bin
-              "/Users/fredaxin/projects/oopenai/oopenai/test/test_files/test_file.jsonl"
-              In_channel.input_all
-          in
-          let purpose = "It just doesn't matter." in
-          let+ resp = API.create_file ~file ~purpose in
-          String.equal resp.purpose purpose
+          fun () ->
+            let* file =
+              Lwt_io.(with_file ~mode:Input)
+                "./test_files/test_file.jsonl"
+                Lwt_io.read
+            in
+            let purpose = "It just doesn't matter." in
+            let+ resp = API.create_file ~file ~purpose in
+            String.equal resp.purpose purpose
         end )
   ; ( `Disabled
     , test
         "can create_moderation"
         begin
-          let create_moderation_request_t =
-            Create_moderation_request.create
-              [ "I want to kill them or give them cake!" ]
-          in
-          let+ resp = API.create_moderation ~create_moderation_request_t in
-          List.length resp.results != 0
+          fun () ->
+            let create_moderation_request_t =
+              Create_moderation_request.create
+                [ "I want to kill them or give them cake!" ]
+            in
+            let+ resp = API.create_moderation ~create_moderation_request_t in
+            List.length resp.results != 0
         end )
   ; ( `Disabled
     , test
         "can list_models"
         begin
-          let* resp = API.list_models () in
-          let* () = Lwt_io.printl "Model ids:" in
-          let* () =
-            Lwt_list.iter_s (fun (m : Model.t) -> Lwt_io.printl m.id) resp.data
-          in
-          let+ () = Lwt_io.(flush stdout) in
-          List.length resp.data > 0
+          fun () ->
+            let* resp = API.list_models () in
+            let* () = Lwt_io.printl "Model ids:" in
+            let* () =
+              Lwt_list.iter_s
+                (fun (m : Model.t) -> Lwt_io.printl m.id)
+                resp.data
+            in
+            let+ () = Lwt_io.(flush stdout) in
+            List.length resp.data > 0
         end )
   ; ( `Disabled
     , test
         "can retrieve_model"
         begin
-          let+ resp = API.retrieve_model ~model:"text-davinci-003" in
-          String.equal resp.id "text-davinci-003"
+          fun () ->
+            let+ resp = API.retrieve_model ~model:"text-davinci-003" in
+            String.equal resp.id "text-davinci-003"
         end )
   ]
   |> List.filter_map (function
