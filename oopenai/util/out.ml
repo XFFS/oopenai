00ec66c546f373f75c758546cd8ad431d9c43bb8
Shon Feder <shon.feder@gmail.com>
2023-03-17 09:46:35 -0400

    Start exploring code generation

---
+++ oopenai/util/out.ml
@@ -0,0 +1,6 @@
+module OpenAI_api =
+  struct
+    let start_chat = Client.request "chat" "mydata"
+    let end_chat = Client.request "chat/end" "foo"
+    let completion = Client.request "complete" "mycompletiondata"
+  end
