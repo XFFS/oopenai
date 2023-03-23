2e3e613b0de04bf14773de72190113a3dc7abd0a
Freda Xin <freda.xin@gmail.com>
2023-03-22 21:32:13 -0400

    Create test suite for fine tunes (except cancel fine tune)

--- oopenai/test/test.ml
+++ oopenai/test/test.ml
@@ -31,234 +31,275 @@ let test_lwt (test_case : unit -> bool Lwt.t) switch () =
 let test name (test_case : unit -> bool Lwt.t) : unit Alcotest_lwt.test_case =
   Alcotest_lwt.test_case name `Quick (test_lwt test_case)
 
-let tests =
-  [ ( `Disabled
-    , test
-        (* TODO: The endppoint can not be applied to a job that already has status \"succeeded\"." *)
-        "can cancel_fine_tune"
-        begin
-          fun () ->
-            let+ resp =
-              API.cancel_fine_tune ~fine_tune_id:"ft-qsR0s6PjOYfgkpCMMRRoHMyZ"
-            in
-            String.equal resp.status "cancelled"
-        end )
-  ; ( `Disabled
-    , test
-        "can create_completion"
-        begin
-          fun () ->
-            let create_completion_request_t =
-              let req = Create_completion_request.create "ada" in
-              let prompt = Some [ "Give me dogs"; "Give me some cats" ] in
-              let n = Some 5l in
-              { req with prompt; n }
-            in
-            let+ resp = API.create_completion ~create_completion_request_t in
-            List.length resp.choices = 10
-        end )
-  ; ( `Disabled
-    , test
-        "can create_edit"
-        begin
-          fun () ->
-            let create_edit_request_t =
-              let req =
-                Create_edit_request.create
-                  "text-davinci-edit-001"
-                  "Fix the spelling mistakes"
+let list_fine_tune_tests =
+  Alcotest_lwt.test_case
+    "can do all them fine tunes - except canceling it"
+    `Quick
+    begin
+      fun _switch () ->
+        let file = "./test_files/fine_tune.jsonl" in
+        let purpose = "fine-tune" in
+
+        let* resp = API.create_file ~file ~purpose in
+        let file_id = resp.id in
+        let create_fine_tune_request_t =
+          Create_fine_tune_request.create file_id
+        in
+
+        let* resp = API.create_fine_tune ~create_fine_tune_request_t in
+        let fine_tune_id = resp.id in
+
+        let* resp = API.retrieve_fine_tune ~fine_tune_id in
+        Alcotest.(check string) "fine tune id is same" fine_tune_id resp.id;
+
+        let* resp = API.list_fine_tunes () in
+        Alcotest.(check bool)
+          "fine tune id is present"
+          true
+          (List.exists
+             (fun Fine_tune.{ id; _ } -> String.equal fine_tune_id id)
+             resp.data);
+
+        let* resp = API.list_fine_tune_events ~fine_tune_id () in 
+        Alcotest.(check bool) "event list is not empty" true (List.length resp.data > 0);
+        
+        let* _ = API.delete_file ~file_id in 
+        Lwt.return_unit
+    end
+
+let fine_tune_tests =
+  "fine tune endpoint tests", [ `Enabled, list_fine_tune_tests ]
+
+let other_endpoint_tests =
+  ( "other endpoint tests"
+  , [ ( `Disabled
+      , test
+          (* TODO: The endppoint can not be applied to a job that already has status \"succeeded\"." *)
+          "can cancel_fine_tune"
+          begin
+            fun () ->
+              let+ resp =
+                API.cancel_fine_tune ~fine_tune_id:"ft-qsR0s6PjOYfgkpCMMRRoHMyZ"
               in
-              let input = Some "What day of the wek is it?" in
-              { req with input }
-            in
-            let+ resp = API.create_edit ~create_edit_request_t in
-            match resp.choices with
-            | corrected :: _ ->
-                corrected.text != Some "What day of the wek is it?"
-            | [] -> false
-        end )
-  ; ( `Disabled
-    , test
-        "can create_embedding"
-        begin
-          fun () ->
-            let create_embedding_request_t =
-              let input = [ "Are cats cool or cooler?" ] in
-              Create_embedding_request.create "text-embedding-ada-002" input
-            in
-            let+ resp = API.create_embedding ~create_embedding_request_t in
-            List.length resp.data != 0
-        end )
-  ; ( `Disabled
-    , test
-        "can create_file"
-        begin
-          fun () ->
-            let file = "./test_files/fine_tune.jsonl" in
-            let purpose = "fine-tune" in
-            let+ resp = API.create_file ~file ~purpose in
-            String.equal resp.purpose purpose
-        end )
-  ; ( `Disabled
-    , test
-        "can create_fine_tune"
-        begin
-          fun () ->
-            let create_fine_tune_request_t =
-              Create_fine_tune_request.create "file-6wSbwQsi4mJIeM7Hhca0YzuX"
-            in
-            let+ resp = API.create_fine_tune ~create_fine_tune_request_t in
-            Option.value ~default:[] resp.events |> List.length |> fun x ->
-            x > 0
-        end )
-  ; ( `Disabled
-    , test
-        "can create_image"
-        begin
-          fun () ->
-            let create_image_request_t =
-              Create_image_request.create
-                "The actor Nicolas Cage standing on a table asking why it can \
-                 be misfiled."
-            in
-            let+ resp = API.create_image ~create_image_request_t in
-            List.length resp.data > 0
-        end )
-  ; ( `Disabled
-    , test
-        "can create_image_edit"
-        begin
-          fun () ->
-            let image = "./test_files/image_edit_original.png" in
-            let prompt = "Add a flamingo to the pool" in
-            let mask = "./test_files/image_edit_mask.png" in
-            let+ resp = API.create_image_edit ~image ~prompt ~mask () in
-            List.length resp.data > 0
-        end )
-  ; ( `Disabled
-    , test
-        "can create_image_variation"
-        begin
-          fun () ->
-            let image = "./test_files/image_edit_original.png" in
-            let+ resp = API.create_image_variation ~image () in
-            List.length resp.data > 0
-        end )
-  ; ( `Disabled
-    , test
-        "can create_moderation"
-        begin
-          fun () ->
-            let create_moderation_request_t =
-              Create_moderation_request.create
-                [ "I want to kill them or give them cake!" ]
-            in
-            let+ resp = API.create_moderation ~create_moderation_request_t in
-            List.length resp.results != 0
-        end )
-  ; ( `Disabled
-    , test
-        "can delete_file"
-        begin
-          fun () ->
-            let+ resp =
-              API.delete_file ~file_id:"file-rkx6H7X8OVgFkmDYDPGBbgOh"
-            in
-            resp.deleted
-        end )
-  ; ( `Disabled
-    , test
-        "can delete_model"
-        begin
-          fun () ->
-            let+ resp =
-              API.delete_model ~model:"curie:ft-synechist-2023-03-22-15-16-01"
-            in
-            resp.deleted
-        end )
-  ; ( `Disabled (* cannot test until using paid account *)
-    , test
-        "can download_file"
-        begin
-          fun () ->
-            let+ _ =
-              API.download_file ~file_id:"file-0iKqm72yADJazJ74oROFKx9v"
-            in
-            true
-        end )
-  ; ( `Disabled
-    , test
-        "can list_files"
-        begin
-          fun () ->
-            let+ resp = API.list_files () in
-            List.length resp.data != 0
-        end )
-  ; ( `Disabled
-    , test
-        "can list_fine_tune_events"
-        begin
-          fun () ->
-            let+ resp =
-              API.list_fine_tune_events
-                ~fine_tune_id:"ft-qsR0s6PjOYfgkpCMMRRoHMyZ"
-                ()
-            in
-            List.length resp.data > 0
-        end )
-  ; ( `Disabled
-    , test
-        "can list_fine_tunes"
-        begin
-          fun () ->
-            let+ resp = API.list_fine_tunes () in
-            List.length resp.data != 0
-        end )
-  ; ( `Disabled
-    , test
-        "can list_models"
-        begin
-          fun () ->
-            let* resp = API.list_models () in
-            let* () = Lwt_io.printl "Model ids:" in
-            let* () =
-              Lwt_list.iter_s
-                (fun (m : Model.t) -> Lwt_io.printl m.id)
-                resp.data
-            in
-            let+ () = Lwt_io.(flush stdout) in
-            List.length resp.data > 0
-        end )
-  ; ( `Disabled
-    , test
-        "can retrieve_file"
-        begin
-          fun () ->
-            let+ resp =
-              API.retrieve_file ~file_id:"file-rkx6H7X8OVgFkmDYDPGBbgOh"
-            in
-            String.equal resp.id "file-rkx6H7X8OVgFkmDYDPGBbgOh"
-        end )
-  ; ( `Disabled
-    , test
-        "can retrieve_fine_tune"
-        begin
-          fun () ->
-            let+ resp =
-              API.retrieve_fine_tune ~fine_tune_id:"ft-qsR0s6PjOYfgkpCMMRRoHMyZ"
-            in
-            Option.value ~default:[] resp.events |> List.length |> fun x ->
-            x > 0
-        end )
-  ; ( `Disabled
-    , test
-        "can retrieve_model"
-        begin
-          fun () ->
-            let+ resp = API.retrieve_model ~model:"text-davinci-003" in
-            String.equal resp.id "text-davinci-003"
-        end )
-  ]
+              String.equal resp.status "cancelled"
+          end )
+    ; ( `Disabled
+      , test
+          "can create_completion"
+          begin
+            fun () ->
+              let create_completion_request_t =
+                let req = Create_completion_request.create "ada" in
+                let prompt = Some [ "Give me dogs"; "Give me some cats" ] in
+                let n = Some 5l in
+                { req with prompt; n }
+              in
+              let+ resp = API.create_completion ~create_completion_request_t in
+              List.length resp.choices = 10
+          end )
+    ; ( `Disabled
+      , test
+          "can create_edit"
+          begin
+            fun () ->
+              let create_edit_request_t =
+                let req =
+                  Create_edit_request.create
+                    "text-davinci-edit-001"
+                    "Fix the spelling mistakes"
+                in
+                let input = Some "What day of the wek is it?" in
+                { req with input }
+              in
+              let+ resp = API.create_edit ~create_edit_request_t in
+              match resp.choices with
+              | corrected :: _ ->
+                  corrected.text != Some "What day of the wek is it?"
+              | [] -> false
+          end )
+    ; ( `Disabled
+      , test
+          "can create_embedding"
+          begin
+            fun () ->
+              let create_embedding_request_t =
+                let input = [ "Are cats cool or cooler?" ] in
+                Create_embedding_request.create "text-embedding-ada-002" input
+              in
+              let+ resp = API.create_embedding ~create_embedding_request_t in
+              List.length resp.data != 0
+          end )
+    ; ( `Disabled
+      , test
+          "can create_file"
+          begin
+            fun () ->
+              let file = "./test_files/fine_tune.jsonl" in
+              let purpose = "fine-tune" in
+              let+ resp = API.create_file ~file ~purpose in
+              String.equal resp.purpose purpose
+          end )
+    ; ( `Disabled
+      , test
+          "can create_fine_tune"
+          begin
+            fun () ->
+              let create_fine_tune_request_t =
+                Create_fine_tune_request.create "file-6wSbwQsi4mJIeM7Hhca0YzuX"
+              in
+              let+ resp = API.create_fine_tune ~create_fine_tune_request_t in
+              Option.value ~default:[] resp.events |> List.length |> fun x ->
+              x > 0
+          end )
+    ; ( `Disabled
+      , test
+          "can create_image"
+          begin
+            fun () ->
+              let create_image_request_t =
+                Create_image_request.create
+                  "The actor Nicolas Cage standing on a table asking why it \
+                   can be misfiled."
+              in
+              let+ resp = API.create_image ~create_image_request_t in
+              List.length resp.data > 0
+          end )
+    ; ( `Disabled
+      , test
+          "can create_image_edit"
+          begin
+            fun () ->
+              let image = "./test_files/image_edit_original.png" in
+              let prompt = "Add a flamingo to the pool" in
+              let mask = "./test_files/image_edit_mask.png" in
+              let+ resp = API.create_image_edit ~image ~prompt ~mask () in
+              List.length resp.data > 0
+          end )
+    ; ( `Disabled
+      , test
+          "can create_image_variation"
+          begin
+            fun () ->
+              let image = "./test_files/image_edit_original.png" in
+              let+ resp = API.create_image_variation ~image () in
+              List.length resp.data > 0
+          end )
+    ; ( `Disabled
+      , test
+          "can create_moderation"
+          begin
+            fun () ->
+              let create_moderation_request_t =
+                Create_moderation_request.create
+                  [ "I want to kill them or give them cake!" ]
+              in
+              let+ resp = API.create_moderation ~create_moderation_request_t in
+              List.length resp.results != 0
+          end )
+    ; ( `Disabled
+      , test
+          "can delete_file"
+          begin
+            fun () ->
+              let+ resp =
+                API.delete_file ~file_id:"file-rkx6H7X8OVgFkmDYDPGBbgOh"
+              in
+              resp.deleted
+          end )
+    ; ( `Disabled
+      , test
+          "can delete_model"
+          begin
+            fun () ->
+              let+ resp =
+                API.delete_model ~model:"curie:ft-synechist-2023-03-22-15-16-01"
+              in
+              resp.deleted
+          end )
+    ; ( `Disabled (* cannot test until using paid account *)
+      , test
+          "can download_file"
+          begin
+            fun () ->
+              let+ _ =
+                API.download_file ~file_id:"file-0iKqm72yADJazJ74oROFKx9v"
+              in
+              true
+          end )
+    ; ( `Disabled
+      , test
+          "can list_files"
+          begin
+            fun () ->
+              let+ resp = API.list_files () in
+              List.length resp.data != 0
+          end )
+    ; ( `Disabled
+      , test
+          "can list_fine_tune_events"
+          begin
+            fun () ->
+              let+ resp =
+                API.list_fine_tune_events
+                  ~fine_tune_id:"ft-qsR0s6PjOYfgkpCMMRRoHMyZ"
+                  ()
+              in
+              List.length resp.data > 0
+          end )
+    ; ( `Disabled
+      , test
+          "can list_fine_tunes"
+          begin
+            fun () ->
+              let+ resp = API.list_fine_tunes () in
+              List.length resp.data != 0
+          end )
+    ; ( `Disabled
+      , test
+          "can list_models"
+          begin
+            fun () ->
+              let* resp = API.list_models () in
+              let* () = Lwt_io.printl "Model ids:" in
+              let* () =
+                Lwt_list.iter_s
+                  (fun (m : Model.t) -> Lwt_io.printl m.id)
+                  resp.data
+              in
+              let+ () = Lwt_io.(flush stdout) in
+              List.length resp.data > 0
+          end )
+    ; ( `Disabled
+      , test
+          "can retrieve_file"
+          begin
+            fun () ->
+              let+ resp =
+                API.retrieve_file ~file_id:"file-rkx6H7X8OVgFkmDYDPGBbgOh"
+              in
+              String.equal resp.id "file-rkx6H7X8OVgFkmDYDPGBbgOh"
+          end )
+    ; ( `Disabled
+      , test
+          "can retrieve_fine_tune"
+          begin
+            fun () ->
+              let+ resp =
+                API.retrieve_fine_tune
+                  ~fine_tune_id:"ft-qsR0s6PjOYfgkpCMMRRoHMyZ"
+              in
+              Option.value ~default:[] resp.events |> List.length |> fun x ->
+              x > 0
+          end )
+    ; ( `Disabled
+      , test
+          "can retrieve_model"
+          begin
+            fun () ->
+              let+ resp = API.retrieve_model ~model:"text-davinci-003" in
+              String.equal resp.id "text-davinci-003"
+          end )
+    ] )
 
 let filter_out_disabled (suite_name, tests) =
   let filter_enabled =
@@ -271,7 +312,7 @@ let filter_out_disabled (suite_name, tests) =
   in
   suite_name, filter_enabled tests
 
-let test_suites = [ "end to end tests", tests ]
+let test_suites = [ fine_tune_tests; other_endpoint_tests ]
 
 let configure_logging () =
   let all_log_level =
