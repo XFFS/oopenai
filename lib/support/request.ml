c02e7a39ada472a5c46a2a04889378f0dd8bafac
Shon Feder <shon.feder@gmail.com>
2023-04-05 22:37:49 -0400

    Use multipart_form library

--- lib/support/request.ml
+++ lib/support/request.ml
@@ -1,26 +1,26 @@
-module type Auth = sig 
+module type Auth = sig
   val api_key : string
-  val org_id : string option 
-end 
+  val org_id : string option
+end
 
-
-module Make (Config: Auth) = struct
-
-  let api_key = ""
+module Make (Config : Auth) = struct
   let base_url = "https://api.openai.com/v1"
 
   (* Authentication was not added by the OpenAPI generator,
      but a bearer token is required and an organization id is
      optionally supported. *)
-  let default_headers = 
-    let org_id_header = match Config.org_id with
+  let add_default_headers hs =
+    let org_id_header =
+      match Config.org_id with
       | None -> []
-      | Some id -> ["OpenAI-Organization", id]
+      | Some id -> [ "OpenAI-Organization", id ]
     in
-    Cohttp.Header.of_list ( 
-      [ "Authorization", "Bearer "^Config.api_key
-      ] @ org_id_header
-    )
+    let defaults =
+      [ "Authorization", "Bearer " ^ Config.api_key ] @ org_id_header
+    in
+    Cohttp.Header.add_list hs defaults
+
+  let default_headers = Cohttp.Header.init () |> add_default_headers
 
   let json_content_headers =
     Cohttp.Header.add default_headers "Content-Type" "application/json"
@@ -31,9 +31,7 @@ module Make (Config: Auth) = struct
     | None -> default
 
   let build_uri operation_path = Uri.of_string (base_url ^ operation_path)
-
-  let add_string_header headers key value =
-    Cohttp.Header.add headers key value
+  let add_string_header headers key value = Cohttp.Header.add headers key value
 
   let add_string_header_multi headers key values =
     Cohttp.Header.add_multi headers key values
@@ -57,27 +55,30 @@ module Make (Config: Auth) = struct
 
   let write_as_json_body to_json payload = write_json_body (to_json payload)
 
-  let handle_response resp ?body on_success_handler  =
+  let handle_response resp ?body on_success_handler =
     match Cohttp_lwt.Response.status resp with
     | #Cohttp.Code.success_status -> on_success_handler ()
-    | s -> 
-      let open Lwt.Syntax in
-      Logs.err (fun f -> f "%a" Cohttp_lwt.Response.pp_hum resp);
-      let* () = match body with
-      | None -> Lwt.return ()
-      | Some b ->
-        let+ body_str = Cohttp_lwt.Body.to_string b in
-        Logs.err (fun f -> f "%s" body_str)
-      in
-      failwith ("Server responded with status " ^ Cohttp.Code.(reason_phrase_of_code (code_of_status s)))
+    | s ->
+        let open Lwt.Syntax in
+        Logs.err (fun f -> f "%a" Cohttp_lwt.Response.pp_hum resp);
+        let* () =
+          match body with
+          | None -> Lwt.return ()
+          | Some b ->
+              let+ body_str = Cohttp_lwt.Body.to_string b in
+              Logs.err (fun f -> f "%s" body_str)
+        in
+        failwith
+          ("Server responded with status "
+          ^ Cohttp.Code.(reason_phrase_of_code (code_of_status s)))
 
   let handle_unit_response resp = handle_response resp (fun () -> Lwt.return ())
 
   let read_json_body resp body =
     handle_response resp ~body (fun () ->
-      let open Lwt.Syntax in
-      let+ resp_string = Cohttp_lwt.Body.to_string body in
-      Yojson.Safe.from_string resp_string)
+        let open Lwt.Syntax in
+        let+ resp_string = Cohttp_lwt.Body.to_string body in
+        Yojson.Safe.from_string resp_string)
 
   let read_json_body_as of_json resp body =
     Lwt.(read_json_body resp body >|= of_json)
@@ -92,11 +93,14 @@ module Make (Config: Auth) = struct
     Lwt.(read_json_body resp body >|= Yojson.Safe.Util.to_assoc)
 
   let read_json_body_as_map_of of_json resp body =
-    Lwt.(read_json_body_as_map resp body >|= List.map (fun (s, v) -> (s, of_json v)))
+    Lwt.(
+      read_json_body_as_map resp body >|= List.map (fun (s, v) -> s, of_json v))
 
   let replace_string_path_param uri param_name param_value =
     let regexp = Str.regexp (Str.quote ("{" ^ param_name ^ "}")) in
-    let path = Str.global_replace regexp param_value (Uri.pct_decode (Uri.path uri)) in
+    let path =
+      Str.global_replace regexp param_value (Uri.pct_decode (Uri.path uri))
+    in
     Uri.with_path uri path
 
   let replace_path_param uri param_name to_string param_value =
@@ -117,16 +121,30 @@ module Make (Config: Auth) = struct
   let init_form_encoded_body () = ""
 
   let add_form_encoded_body_param params param_name to_string param_value =
-    let new_param_enc = Printf.sprintf {|%s=%s|} (Uri.pct_encode param_name) (Uri.pct_encode (to_string param_value)) in
-    if params = ""
-    then new_param_enc
-    else Printf.sprintf {|%s&%s|} params new_param_enc
+    let new_param_enc =
+      Printf.sprintf
+        {|%s=%s|}
+        (Uri.pct_encode param_name)
+        (Uri.pct_encode (to_string param_value))
+    in
+    if params = "" then
+      new_param_enc
+    else
+      Printf.sprintf {|%s&%s|} params new_param_enc
 
   let add_form_encoded_body_param_list params param_name to_string new_params =
-    add_form_encoded_body_param params param_name (String.concat ",") (to_string new_params)
+    add_form_encoded_body_param
+      params
+      param_name
+      (String.concat ",")
+      (to_string new_params)
 
-  let maybe_add_form_encoded_body_param params param_name to_string param_value =
-    option_fold (add_form_encoded_body_param params param_name to_string) params param_value
+  let maybe_add_form_encoded_body_param params param_name to_string param_value
+      =
+    option_fold
+      (add_form_encoded_body_param params param_name to_string)
+      params
+      param_value
 
   let finalize_form_encoded_body body = Cohttp_lwt.Body.of_string body
 end
