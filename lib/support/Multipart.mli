8790a33e85c2ca238ec05792bae30e6f1e41462d
Shon Feder <shon.feder@gmail.com>
2024-01-19 21:33:57 -0500

    Get rid of previously generated client

--- lib/support/Multipart.mli
+++
@@ -1,3 +0,0 @@
-val part_of_file : name:string -> source:string -> filename:string -> Multipart_form.part Lwt.t
-val part_of_string : name:string -> content:string -> Multipart_form.part
-val form : ?header:Multipart_form.Header.t -> ?boundary:string -> Multipart_form.part list -> Multipart_form.multipart
