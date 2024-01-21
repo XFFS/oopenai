7d768c909013525b3e7ddb625191959e486e2453
Shon Feder <shon.feder@gmail.com>
2024-01-20 23:26:35 -0500

    Port examples for use with Oooapi client

--- examples/examples.ml
+++ examples/examples.ml
@@ -1,40 +1,56 @@
-open Oopenai
+module Ooo = Oooapi_lib
+open Oopenai.Data
 
 (* Configure autherization thru environment variables *)
-module Config : Request.Auth = struct
-  let api_key = Sys.getenv "OPENAI_API_KEY"
-  let org_id = None
+module Config : Ooo.Config = struct
+  let bearer_token = Sys.getenv_opt "OPENAI_API_KEY"
+  let default_headers = None
 end
 
-module API = Open_ai_api.Make (Config)
+module API = Oopenai.Make (Ooo.Cohttp_client) (Config)
 
 (* A utility function *)
 let print_option = function
   | Some v -> Lwt_io.printl v
   | None -> Lwt.return_unit
 
-let main () =
-  let open Lwt.Syntax in
+open Lwt_result.Syntax
+
+let main =
   (* List models *)
   let* resp = API.list_models () in
   let* () =
-    Lwt_list.iter_s (fun (m : Model.t) -> Lwt_io.printl m.id) resp.data
+    resp.data
+    |> Lwt_list.iter_s (fun (m : Model.t) -> Lwt_io.printl m.id)
+    |> Lwt_result.ok
   in
 
   (* Create completion *)
-  let create_completion_request_t =
-    let req = Create_completion_request.create "ada" in
-    let prompt = Some [ "Give me dogs"; "Give me some cats" ] in
-    let n = Some 5l in
-    { req with prompt; n }
+  let* resp =
+    API.create_completion
+    @@ CreateCompletionRequest.make
+         ~model:"davinci-002"
+         ~prompt:"Give me dogs and cats"
+         ~n:5
+         ()
   in
-  let* resp = API.create_completion ~create_completion_request_t in
   let+ () =
-    Lwt_list.iter_s
-      (fun (choice:Create_completion_response_choices_inner.t) ->
-        print_option choice.text)
-      resp.choices
+    resp.choices
+    |> Lwt_list.iter_s (fun (choice : CreateCompletionResponse.choices_item) ->
+           Lwt_io.printl choice.choices_item_text)
+    |> Lwt_result.ok
   in
   ()
 
-let () = Lwt_main.run (main ())
+let _ =
+  match Lwt_main.run main with
+  | Ok () -> ()
+  | Error (`Deseriaization (resp, error)) ->
+      Printf.eprintf "Error %s when deserializing response %s" error resp;
+      exit 1
+  | Error (`Request (code, msg)) ->
+      Printf.eprintf
+        "Error respnose %d from server with message %s"
+        (Cohttp.Code.code_of_status code)
+        msg;
+      exit 1
